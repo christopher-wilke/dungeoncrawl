fde73f65f9a03b031b9a3393e6d20756b0f0f563
CHRISTOPHER WILKE <microsoft.wilke@extaccount.com>
2021-09-08 11:38:55 +0200

    WIP: Rust ECS

---
+++ src/components.rs
@@ -0,0 +1,15 @@
+pub use crate::prelude::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType
+}
+
+pub struct Player;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Enemy;
+
+#[derive(Clone, Copy, Debug,PartialEq)]
+pub struct MovingRandomly;
---
+++ src/systems/mod.rs
@@ -0,0 +1,52 @@
+mod player_input;
+mod map_render;
+mod entity_render;
+mod collisions;
+mod random_move;
+mod end_turn;
+
+use crate::prelude::*;
+
+use self::random_move::random_move;
+
+pub fn build_input_scheduler() -> Schedule {
+    Schedule::builder()
+                .add_system(player_input::player_input_system())
+                .flush()
+                .add_system(map_render::map_render_system())
+                .add_system(entity_render::entity_render_system())
+                .build()
+}
+
+pub fn build_player_scheduler() -> Schedule {
+    Schedule::builder()
+                .add_system(collisions::collisions_system())
+                .flush()
+                .add_system(map_render::map_render_system())
+                .add_system(entity_render::entity_render_system())
+                .add_system(end_turn::end_turn_system())
+                .build()
+}
+
+pub fn build_monster_scheduler() -> Schedule {
+    Schedule::builder()
+                .add_system(random_move::random_move_system())
+                .flush()
+                .add_system(collisions::collisions_system())
+                .flush()
+                .add_system(map_render::map_render_system())
+                .add_system(entity_render::entity_render_system())
+                .add_system(end_turn::end_turn_system())
+                .build()
+}
+
+pub fn build_scheduler() -> Schedule {
+    Schedule::builder()
+    .add_system(player_input::player_input_system())
+    .add_system(collisions::collisions_system())
+    .flush()
+    .add_system(map_render::map_render_system())
+    .add_system(entity_render::entity_render_system())
+    .add_system(random_move::random_move_system())
+    .build()
+}
